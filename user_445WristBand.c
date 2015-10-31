efc06ada7576a3251f82e3052fdb1af83111a7b1
Status <bkuo@illinois.edu>
2015-10-31 13:07:53 -0500

    made some SPI changes, untested and adding some base GSCLK code, currently Hobby PWM setup will update later

--- user_445WristBand.c
+++ user_445WristBand.c
@@ -99,6 +99,13 @@ void main(void) {
 	TACTL = TASSEL_2 + MC_1; // source SMCLK, up mode
 
 	// Timer A GSCLK Setup
+	// Hobby Servo Set Up, Should Change Later
+	TA1CCR0 = 40000;                             // PWM Period
+	TA1CCTL1 = OUTMOD_7;                         // TA1CCR1 reset/set
+	TA1CCTL2 = OUTMOD_7;                         // TA1CCR2 reset/set
+	TA1CCR1 = 3400;                   // TA1CCR1 PWM duty cycle about 50% initially of max pulse width
+	TA1CCR2 = 3400;                   // TA1CCR2 PWM duty cycle about 50% initially of max pulse width
+	TA1CTL = TASSEL_2 + ID_3 + MC_1;                  // SMCLK, up mode, ID - sclock divider for 8
 
 
 	//SPI Config - Might move to new file in future
@@ -107,15 +114,25 @@ void main(void) {
 	UCB0CTL0 = UCCKPH + UCMSB + UCMST + UCSYNC; // 3-pin, 8-bit SPI master
 	UCB0CTL1 |= UCSSEL_2; // SMCLK
     // UCB0BR0 |= 0x01; // 1:1
-    UCB0BR0 = 80;
+    UCB0BR0 = 80;//may want to adjust
     UCB0BR1 = 0;
     UCB0CTL1 &= ~UCSWRST; // clear SW
 
 	Init_UART(115200,1);	// Initialize UART for 9600 baud serial communication
 
-	_BIS_SR(GIE); 		// Enable global interrupt
+	_BIS_SR(GIE); 		// Enable global ianterrupt
 
 //	UART_printf("AT+CIPMUX=1\r\nAT+CIPSERVER=1\r\n");
+
+	int i;
+	for (i = 0; i < NUMBER_OF_MOTORS; i++)
+	{
+		motors[i/2] = 4095;
+	}
+	for (i = 0; i < NUMBER_OF_MOTORS; i++)
+	{
+		motors[(i/2)+1] = 4095;
+	}
 
 	while(1) {
 
@@ -131,9 +148,9 @@ void main(void) {
 //			UART_printf("AT+CIPMUX=1\r\nAT+CIPSERVER=1\r\n");
 			//jank work around because printf wasn't working with the longer strings
 			if((timecnt>=2500)&&(timecnt<3000))
-				UART_printf("AT+CIPMUX=1\r\n");//AT+CIPSERVER=1\r\n");
+				 UART_printf("AT+CIPMUX=1\r\n");//AT+CIPSERVER=1\r\n");
 			if((timecnt>=3000)&&(timecnt<3500))
-				UART_printf("AT+CIPSERVER=1\r\n");
+				 UART_printf("AT+CIPSERVER=1\r\n");
 			if(timecnt>=4000)
 			{
 				updateTLC_array();
@@ -141,12 +158,12 @@ void main(void) {
 			}
 
 //			UART_printf("Hello %d\n\r",(int)(timecnt/500));
-			int i;
-			for (i = 0; i < NUMBER_OF_MOTORS; i++)
-			{
-				motors[i] = timecnt;
-			}
-			newprint = 0;
+//			int i;
+//			for (i = 0; i < NUMBER_OF_MOTORS; i++)
+//			{
+//				motors[i] = timecnt;
+//			}
+//			newprint = 0;
 		}
 
 	}
@@ -208,16 +225,18 @@ __interrupt void USCI0TX_ISR(void) {
 	}
 
 	if(IFG2&UCB0TXIFG) {	// USCI_B0 requested TX interrupt (UCB0TXBUF is empty)
-		if (spi_index >= SPI_BUFF_SIZE)
+		if (spi_index < 0)
 		{
 			P2OUT |= XLAT;
 			P2OUT &= ~XLAT;
-			spi_index = 0;
+			P2OUT |= XLAT;
+			P2OUT &= ~XLAT;
+			spi_index = NUMBER_OF_MOTORS - 2;
 		}
 		else
 		{		
 			UCB0TXBUF = spi_sout_buff[spi_index];
-			spi_index++;
+			spi_index--;
 		}
 		IFG2 &= ~UCB0TXIFG;   // clear IFG
 	}
@@ -272,52 +291,6 @@ __interrupt void USCI0RX_ISR(void) {
 			}
 		}
 		else {	// In process of receiving a message		
-			// if (junk_count < 4)//junk IPD,
-			// {
-			// 	recchar = UCA0RXBUF;
-			// 	junk_count++;
-			// 	if ((recchar != 'I')||(recchar != 'P')||(recchar != 'D')||(recchar != ','))//this should ensure the setup responses don't trigger false alarms
-			// 	{
-			// 		started = 0;
-			// 		newmsg = 0;
-			// 		msgindex = 0;
-			// 	}
-			// }
-			// else if (junk_count == 4)//store the connection ID
-			// {
-			// 	recchar = UCA0RXBUF;
-			// 	connection_ID = atoi(&recchar);
-			// 	junk_count++;
-			// }
-			// else if (junk_count == 5)//junk ,
-			// {
-			// 	recchar = UCA0RXBUF;
-			// 	junk_count++;
-			// }
-			// else if (junk_count == 6)//store the length that we should be recieving, need to make sure messages are shorter than 10 characters
-			// {
-			// 	recchar = UCA0RXBUF;
-			// 	receive_length = atoi(&recchar);
-			// 	junk_count++;
-			// }
-			// else if (junk_count == 7)//junk :
-			// {
-			// 	recchar = UCA0RXBUF;
-			// 	junk_count++;
-			// }
-			// else if((msgindex < receive_length) && (msgindex < 255)) {
-			// 	rxbuff[msgindex] = UCA0RXBUF;
-
-			// 	msgindex++;
-			// }
-			// else {	// designated length hit
-			// 	// if(UCA0RXBUF == 255) {	// Message completed
-			// 		newmsg = 1;
-			// 		rxbuff[msgindex] = 255;	// "Null"-terminate the array
-			// 	// }
-			// 	started = 0;
-			// 	msgindex = 0;
-			// }
 			//IMPORTANT: this is not tolerant to 2+ digits
 			error_flag = 0;//changes to 1 if incoming data isn't as expected
 			switch(junk_count)
@@ -357,7 +330,7 @@ __interrupt void USCI0RX_ISR(void) {
 						error_flag = 1;
 					junk_count++;
 				break;
-				case 7:
+				case 7://message length
 					recchar = UCA0RXBUF;
 					receive_length = atoi(&recchar);
 					junk_count++;
@@ -402,6 +375,7 @@ __interrupt void USCI0RX_ISR(void) {
 void updateTLC_array() {
 
 	unsigned char ledCounter = NUMBER_OF_MOTORS >> 1;
+	unsigned char buff_idx = SPI_BUFF_SIZE-1;
 
 	while (ledCounter-- > 0) {
 
@@ -420,15 +394,17 @@ void updateTLC_array() {
 		// while (!(IFG2 & UCB0TXIFG))
 		// 	; // TX buffer ready?
 		//got rid of the while spins because ISR is more efficient I think
-		spi_sout_buff[ledCounter] = motors[i + 1] >> 4;
+		spi_sout_buff[buff_idx] = motors[i + 1] >> 4;
+		buff_idx--;
 		unsigned char unib = motors[i + 1] << 4;
 		unsigned char lnib = (motors[i] >> 8) & 0x0F;
-		spi_sout_buff[ledCounter-1] = unib | lnib;
-		spi_sout_buff[ledCounter-2] = motors[i];
-		ledCounter-=3;
+		spi_sout_buff[buff_idx] = unib | lnib;
+		buff_idx--;
+		spi_sout_buff[buff_idx] = motors[i];
+		buff_idx--;
 	}
 }
 
 void sendTLC_array() {
-	UCB0TXBUF = spi_sout_buff[0];
+	UCB0TXBUF = spi_sout_buff[SPI_BUFF_SIZE-1];
 }
